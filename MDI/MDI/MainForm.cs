5fa5151e126d26afbb001861e0f5e72ba102c125
BHeinen38 <bailey.heinen@proplanner.com>
2022-06-06 15:21:49 -0500

    Added some controls

---
+++ MDI/MDI/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var childForm = new ChildForm();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void cascadeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void tileHorizontallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void tileVerticallyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Undo();
+            }
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Redo();
+            }
+
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Cut();
+            }
+
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Copy();
+            }
+
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.Paste();
+            }
+
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.ActiveMdiChild != null)
+            {
+                ChildForm childForm = (ChildForm)this.ActiveMdiChild;
+                childForm.DocumentTextBox.SelectAll();
+            }
+        }
+    }
+}
